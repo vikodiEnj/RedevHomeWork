b20e70739e0fd94c342239a46b9c2635e0d6242d
Георг <georgerdman@192.168.100.10>
2026-03-13 17:39:44 +0300

    1312

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const swiper = new Swiper('.slider', {
   pagination: {
     el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
   autoplay: {
-    delay: 3000, 
-    disableOnInteraction: false, 
+    delay: 3000,
+    disableOnInteraction: false,
   },
 });
